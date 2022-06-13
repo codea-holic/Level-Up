a8fefe359b8b2479e5495ac7b87ba2b26fce35cd
Ganesh Singh <krganesh1611@gmail.com>
2022-06-13 19:36:25 +0530

    QuickSort General(Valid for any Index)

--- SearchAndSort/BinarySearch.java
+++ SearchAndSort/BinarySearch.java
@@ -1,5 +1,156 @@
-package SearchAndSort;
+// package SearchAndSort;
 
 public class BinarySearch {
-    
+
+    public static void display(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int data) {
+
+        int n = arr.length, si = 0, ei = n - 1;
+
+        while (si <= ei) {
+            int mid = (ei + si) / 2;
+            if (arr[mid] == data) {
+                return mid;
+            } else if (arr[mid] < data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int BinarySearchInRange(int[] arr, int si, int ei, int data) {
+
+        while (si <= ei) {
+            int mid = (ei + si) / 2;
+            if (arr[mid] == data) {
+                return mid;
+            } else if (arr[mid] < data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int firstIdx(int[] arr, int data) {
+        int n = arr.length, si = 0, ei = n - 1;
+        int idx = -1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data) {
+                idx = mid;
+                ei = mid - 1;
+            } else if (arr[mid] < data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+        return idx;
+    }
+
+    public static int lastIdx(int[] arr, int data) {
+        int n = arr.length, si = 0, ei = n - 1;
+        int idx = -1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data) {
+                idx = mid;
+                si = mid + 1;
+            } else if (arr[mid] < data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+        return idx;
+    }
+
+    // 34
+    public static int[] searchRange(int[] arr, int data) {
+        return new int[] { firstIdx(arr, data), lastIdx(arr, data) };
+    }
+
+    // Remember : ending index (ei) is always pointing to the last index of element
+    // and starting index(si) always pointing to the first occrence at the end of
+    // looping... ((arr[ei] <= data < arr[si])) reference Video-1 @ 2:45:00 also
+    // known as (floor <= data < ceil).
+
+    // https://practice.geeksforgeeks.org/problems/search-insert-position-of-k-in-a-sorted-array/1/#
+    static int searchInsertK(int arr[], int N, int data) {
+        int si = 0, ei = N - 1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data) {
+                return mid;
+            } else if (arr[mid] < data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+        return si;
+    }
+
+    /**
+     * This function find where these element to be inserted, array contains
+     * duplicate values.
+     */
+    public static int insertLocation(int[] arr, int data) {
+
+        int n = arr.length, si = 0, ei = n - 1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] <= data)
+                si = mid + 1;
+            else
+                ei = mid - 1;
+        }
+
+        // si - 1 >= 0 && arr[si - 1] == data ? si - 1: si;
+        int insertPos = si;
+        int lastIdx = si - 1;
+        return si;
+    }
+
+    public static int perfectPosOfElement(int[] arr, int data) {
+        int insertPos = insertLocation(arr, data);
+        int lastIdx = insertPos - 1;
+
+        return (lastIdx >= 0 && arr[lastIdx] == data ? lastIdx : insertPos);
+    }
+
+    public static int nearestElement(int[] arr, int data) {
+        int n = arr.length, si = 0, ei = n - 1;
+
+        if (data <= arr[0] || arr[ei] <= data)
+            return data <= arr[0] ? arr[0] : arr[ei];
+
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] <= data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+        // hame ye pata hai ki loop jab break hua h tab (ei) piche hai (si) se...
+        return data - arr[ei] <= arr[si] - data ? arr[ei] ? arr[si];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 3, 3, 9, 12, 12, 12, 19, 19, 19, 19 };
+        // System.out.println(binarySearch(arr, 3));
+        // display(searchRange(arr, 19));
+        System.out.println(nearestElement(arr, 11));
+    }
 }
---
+++ SearchAndSort/QuickSort.java
@@ -0,0 +1,44 @@
+// package SearchAndSort;
+
+public class QuickSort {
+    
+    private static void swap(int[] arr, int pivot, int ei) {
+        int temp = arr[pivot];
+        arr[pivot] = arr[ei];
+        arr[ei] = temp;
+    }
+
+    public static int segregateData(int [] arr, int si, int ei, int pivot){
+
+        swap(arr, pivot, ei);
+        int p = si - 1, itr = si;
+
+        while(itr <= ei){
+            if(arr[itr] <= arr[ei])
+                swap(arr, ++p, itr);
+            itr++;
+        }
+
+        return p;
+    }
+
+    public static void quickSort(int [] arr, int si, int ei){
+
+        if(si > ei) return;
+
+        int pivot = ei;
+        int pIdx = segregateData(arr, si, ei, pivot);
+
+        quickSort(arr, si, pIdx - 1);
+        quickSort(arr, pIdx + 1, ei);
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {-12, 2, 7, 4, 34, 23, 0, 1, -1, -50, 16, 23, 7, 4, 2, 3};
+        quickSort(arr, 0, arr.length - 1);
+
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
